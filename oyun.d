32a0dd6dd8c08c6921d9f012acf8153f32622c10
Salih Dinçer <salihdb@hotmail.com>
2012-08-22 22:46:50 +0300

    karadelik.bmp

--- oyun.d
+++ oyun.d
@@ -22,37 +22,37 @@ void main(){
     sdl2d.init.SDL_Quit();
   }
   SDL_Renderer* sahne;
-  
+
   auto pencere = new Pencere(400, 400, 0);
-       pencere.Kur(sahne, "Deneme Oyunu 1.0");  
+       pencere.Kur(sahne, "Deneme Oyunu 1.0");
 
   auto zemin = new Nesne();
-       zemin.YükleBMP("duvar.bmp", sahne); 
+       zemin.YükleBMP("duvar.bmp", sahne);
 
   auto oyuncu = new Nesne();
        oyuncu.YükleBMP("karadelik.bmp", sahne);
        oyuncu.x = (pencere.w - oyuncu.w) / 2;
-       oyuncu.y = (pencere.h - oyuncu.h) / 2;          
-         
+       oyuncu.y = (pencere.h - oyuncu.h) / 2;
+
   double HIZLAN   = 0.0123456789;
   double YAVAŞLA  = 0.9987654321;
   double nXkonumu = cast(double)oyuncu.x;
   double nYkonumu = cast(double)oyuncu.y;
   double nXivmesi = 0;
   double nYivmesi = 0;
-  
+
   enum çerçeveKalınlığı = 10;
-  
+
   KLAVYE_TUŞLARI tuşları;
   alias tuşları yönTuşu;
-  
-  printf("%s başladı...\n", pencere.Başlığı);    
- 
+
+  printf("%s başladı...\n", pencere.Başlığı);
+
   int x, y;
   pencere.Konumlandır(0, 0);/*
   pencere.Konumu(&x, &y);//*/
   printf("Pencere %d/%d konumundaydı ve\n", x, y);
- 
+
   enum { yapma, yap };
   //pencere.tamEkran(yap);/*
   pencere.Büyüt();//*/
@@ -66,17 +66,17 @@ void main(){
     int clockFPS = (SDL_GetTicks() - startTick)/loopCounter;
     printf("%d(%.1f FPS)                                \r",
            SDL_GetTicks(), 1000.0 / cast(float)clockFPS);
-    //if(xSay == 999) pencere.Küçült();    
+    //if(xSay == 999) pencere.Küçült();
     if(yönTuşu.sol) nXivmesi -= HIZLAN;
     if(yönTuşu.sağ) nXivmesi += HIZLAN;
         nXivmesi *= YAVAŞLA;
-        nXkonumu += nXivmesi;          
-    
+        nXkonumu += nXivmesi;
+
     if(yönTuşu.üst) nYivmesi -= HIZLAN;
     if(yönTuşu.alt) nYivmesi += HIZLAN;
         nYivmesi *= YAVAŞLA;
         nYkonumu += nYivmesi;
- 
+
     if(nXkonumu > cast(double)(pencere.w - oyuncu.w - çerçeveKalınlığı) ||
       (nXkonumu < cast(double)çerçeveKalınlığı)) {
       nXkonumu -= nXivmesi;
@@ -88,13 +88,11 @@ void main(){
       nYivmesi = -nYivmesi * YAVAŞLA;
     }
     oyuncu.x = cast(int)nXkonumu;
-    oyuncu.y = cast(int)nYkonumu;          
-    
-    //SDL_SetRenderDrawColor(sahne, 100, 149, 237, 0xFF);
-    //SDL_RenderClear(sahne);
+    oyuncu.y = cast(int)nYkonumu;
+
     zemin.Yansıt(sahne);
     oyuncu.Yansıt(sahne);
-   
+
     SDL_RenderPresent(sahne);
   } while(tuşları.Denetle());
   pencere.Son();
